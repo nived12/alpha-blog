54c35a7b0f85f3e9c704ea284fb712cbdb4e41aa
Nived H. Vengilat <nivedvengilat@gmail.com>
2020-05-23 18:42:13 -0500

    Show and edit done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,42 @@ class ArticlesController < ApplicationController
         @article = Article.find_by( id: params[:id] )
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params_permitted)
+        if @article.save
+            flash[:notice] = "Article was successfully created."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find_by(id: params[:id])
+    end
+
+    def update
+        @article = Article.find_by(id: params[:id])
+        if @article.update(params_permitted)
+            flash[:notice] = "Article was updated created."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        # @article = Article.find_by(id: params[:id]).destroy
+    end
+
+    private
+    
+    def params_permitted
+        params.require(:article).permit(:title, :description)
+    end
+
 end
